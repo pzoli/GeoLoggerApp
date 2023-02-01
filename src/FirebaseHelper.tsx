33289a4c5e9083d09feea089ec783e1efc3fc890
Papp Zoltán <papp.zoltan@infokristaly.hu>
2023-02-01 15:39:03 +0100

    separate firebase functions

---
+++ src/FirebaseHelper.tsx
@@ -0,0 +1,76 @@
+import UserInfo from './UserInfo';
+import { firebase } from '@react-native-firebase/database';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+
+export default class FirebaseHelper {
+
+    private loginName: string = '';
+    private loginEmail: string = '';
+    private loginPassword: string = '';
+    private key: string = '';
+
+    private database = firebase.app()
+        .database('https://geologger-b9659-default-rtdb.europe-west1.firebasedatabase.app');
+
+    constructor() {
+        this.loginEmail = 'orangepi.infokristaly@gmail.com';
+        this.loginName = 'Papp Zoltán';
+        this.loginPassword = 'test1234';
+
+    }
+
+    public async recordLocatoinOnFirebaseDB(latitude: number, longitude: number) {
+        console.log(`Record key=[${this.key}]`);
+        const reference = this.database.ref('/geolocations/users/' + this.key);
+        reference.update({
+            latitude: latitude,
+            longitude: longitude
+        }).then(() => { console.log("Location recorded.") });
+    }
+
+    public sigiIn() {
+        return auth().signInWithEmailAndPassword(this.loginEmail, this.loginPassword)
+    }
+
+    public createUser() {
+        return auth().createUserWithEmailAndPassword(this.loginEmail, this.loginPassword)
+    }
+
+    public async reqireKey() {
+        let foundKey: string | null = await this.getLoggedInUsernameKey();
+        if (foundKey === undefined || foundKey === null || foundKey === '') {
+            this.registerUser(new UserInfo(this.loginEmail, this.loginName));
+        } else {
+            this.key = foundKey;
+        }
+    }
+
+    public async registerUser(userInfo: UserInfo) {
+        const userRegiterReference = this.database.ref('/userRegister/').push();
+        await userRegiterReference.set({
+            //userkey: userRegiterReference.key,
+            uid: auth().currentUser?.uid,
+            username: userInfo.username,
+            email: userInfo.email,
+        });
+        this.key = userRegiterReference.key ? userRegiterReference.key : '';
+    }
+
+    public async getLoggedInUsernameKey(): Promise<string> {
+        let result: string = "";
+        let ref = this.database.ref('/userRegister');
+        await ref.once('value')
+            .then(async snapshot => {
+                const val = await snapshot.val()
+                for (const key in val) {
+                    let userInfo = val[key];
+                    if (userInfo.email == auth().currentUser?.email) {
+                        result = key;
+                    }
+                }
+            });
+
+        return await result;
+    }
+
+}
--- src/GeoLoggerScreen.tsx
+++ src/GeoLoggerScreen.tsx
@@ -1,13 +1,8 @@
-import { firebase } from '@react-native-firebase/database';
-import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
-import { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import Geolocation from 'react-native-geolocation-service';
-import { request, PERMISSIONS, check, RESULTS } from 'react-native-permissions';
-import UserInfo from './UserInfo';
-
-const database = firebase.app()
-    .database('https://geologger-b9659-default-rtdb.europe-west1.firebasedatabase.app');
+import { useEffect, useState } from 'react'
+import { Text, View } from 'react-native'
+import Geolocation from 'react-native-geolocation-service'
+import { request, PERMISSIONS, check, RESULTS } from 'react-native-permissions'
+import FirebaseHelper from './FirebaseHelper'
 
 function checkPermission() {
     return new Promise((resolve) => {
@@ -33,38 +28,31 @@ function checkPermission() {
 export default function GeoLoggerScreen() {
     let [latitude, setLatitude] = useState(0);
     let [longitude, setLongitude] = useState(0);
-    let key: string = '';
+    let firebaseHelper: FirebaseHelper = new FirebaseHelper();
 
-    const reqireKey = async () => {
-        let foundKey: string | null = await getLoggedInUsernameKey();
-        if (foundKey === undefined || foundKey === null || foundKey === '') {
-            registerUser(new UserInfo(loginName, 'Papp Zoltán'));
-        } else {
-            key = foundKey;
-        }
+    const startRequest = async () => {
+        firebaseHelper.createUser().then(() => signInAndStart()
+        ).catch((err) => {
+            console.log(err);
+            signInAndStart();
+        })
     }
 
-    const registerUser = async (userInfo: UserInfo) => {
-        const userRegiterReference = database.ref('/userRegister/').push();
-        await userRegiterReference.set({
-            id: userRegiterReference.key,
-            username: userInfo.username,
-            email: userInfo.email,
-        });
-        key = userRegiterReference.key ? userRegiterReference.key : '';
+    const signInAndStart = () => {
+        firebaseHelper.sigiIn().then(() =>
+            firebaseHelper.reqireKey().then(() =>
+                startLocationWatch()
+            ).catch((err) => console.log(err))
+        ).catch((err) => console.log(err))
     }
 
-    const startLocationWatch = async () => {
+    const startLocationWatch = () => {
         Geolocation.watchPosition(
             (position) => {
                 setLatitude(position.coords.latitude);
                 setLongitude(position.coords.longitude);
                 console.log(`Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`);
-                if (key !== null) {
-                    recordLocatoinOnFirebaseDB(key, position.coords.latitude, position.coords.longitude);
-                } else {
-                    console.log('Key is null!');
-                }
+                firebaseHelper.recordLocatoinOnFirebaseDB(position.coords.latitude, position.coords.longitude);
             },
             (error) => {
                 console.log(error.code, error.message);
@@ -74,39 +62,15 @@ export default function GeoLoggerScreen() {
                 interval: 1000,
                 distanceFilter: 0.1
             }
-        );
+        )
 
-    }
-
-    const loginName: string = 'orangepi.infokristaly@gmail.com';
-    const loginPassword: string = 'test1234';
+    };
 
     useEffect(() => {
         checkPermission().then(() => {
-            auth()
-                .createUserWithEmailAndPassword(loginName, loginPassword)
-                .then(() => {
-                    console.log("Registering done ");
-                    sigiIn(loginName, loginPassword);
-                }).catch((err) => {
-                    console.log("Error at registering: " + err);
-                    sigiIn(loginName, loginPassword);
-                }).catch((err) => {
-                    console.log("Error at login: " + err);
-                })
+            startRequest();
         });
-    }, []);
-
-    function sigiIn(loginName: string, loginPassword: string) {
-        auth().signInWithEmailAndPassword(loginName, loginPassword)
-            .then(() => {
-                reqireKey().then(() =>
-                    startLocationWatch()
-                );
-            }).catch((err) => {
-                console.log("Error at login: " + err);
-            })
-    }
+    }, [])
 
     return (
         <View style={{ width: '100%', height: '100%', alignContent: 'center', justifyContent: 'center' }}>
@@ -114,31 +78,4 @@ export default function GeoLoggerScreen() {
             <Text style={{ textAlign: 'center' }}>Latitude: {latitude}</Text>
         </View>
     );
-}
-
-async function recordLocatoinOnFirebaseDB(keyValue: string, latitude: number, longitude: number) {
-    console.log(`Record key=[${keyValue}]`);
-    const reference = database.ref('/geolocations/users/' + keyValue);
-    reference.update({
-        latitude: latitude,
-        longitude: longitude
-    }).then(() => { console.log("Location recorded.") });
-}
-
-async function getLoggedInUsernameKey(): Promise<string> {
-    let result: string = "";
-    let ref = database.ref('/userRegister');
-    await ref.once('value')
-        .then(async snapshot => {
-            const val = await snapshot.val()
-            for (const key in val) {
-                let userInfo = val[key];
-                if (userInfo.email == auth().currentUser?.email) {
-                    result = key;
-                }
-            }
-        });
-
-    return await result;
-}
-
+};
